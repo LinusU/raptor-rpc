362cbf5d57952cc186d3c87fbebf4173580d6369
Linus Unnebäck <linus@folkdatorn.se>
2016-09-22 23:42:42 +0200

    test: add test case for net notifications

--- test/net.js
+++ test/net.js
@@ -19,7 +19,7 @@ describe('net', function () {
       })
 
       resolve(getStream(client).then(function (string) {
-        return JSON.parse(string)
+        return (string === '') ? null : JSON.parse(string)
       }))
     })
   }
@@ -39,6 +39,14 @@ describe('net', function () {
       var obj = { jsonrpc: '2.0', method: request[0], params: request[1], id: id++ }
 
       return send(obj).then(request[2])
+    })
+  })
+
+  it('should handle notifications', function () {
+    var obj = { jsonrpc: '2.0', method: 'ping' }
+
+    return send(obj).then(function (res) {
+      assert.equal(res, null)
     })
   })
 
